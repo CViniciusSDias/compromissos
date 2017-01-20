70933854e24897f4299a61e977fa035191741374
Carlos Santos <carlos.santos@litoralverde.com>
2017-01-20 18:00:03 -0200

    DocBlock adicionado a Ui.java

--- ui/Ui.java
+++ ui/Ui.java
@@ -8,5 +8,18 @@ public interface Ui
     void inserir(Agenda a);
     void listarPorAssunto(Agenda a);
     void remover(Agenda a);
+    /**
+     * Deve exibir um menu e retornar uma das seguintes opções:
+     * 1 - Inserir Compromisso
+     * 2 - Remover Compromisso
+     * 3 - Listar Todos os Compromissos
+     * 4 - Listar Compromissos por Assunto
+     * 5 - Listar Compromissos de Periodo
+     * 6 - Listar Proximos Compromissos
+     * 7 - Listar Compromissos de Periodo
+     * 8 - Sair
+     *
+     * @return int
+     */
     int menu();
 }
